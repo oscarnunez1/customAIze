3e1ab76a036e7ffe963e4a8591d519e45f4769cd
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-04-13 17:12:00 -0400

    import state to app

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { useSnapshot } from 'valtio'
+import state from '../store'
+
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -7,7 +9,11 @@ import {
   slideAnimation
 } from '../config/motion'
 
+
 const Home = () => {
+
+  const snap = useSnapshot(state)
+  
   return (
     <div>Home</div>
   )
