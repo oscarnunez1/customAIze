c4de108a31f658184b5e262e51dc17fb17a3044a
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-04-17 12:23:34 -0400

    add go back button

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -33,9 +33,20 @@ const Customizer = () => {
                   />
                 ))}
               </div>
-            </div>
-
+            </div> 
           </motion.div>
+          <motion.div 
+              className='absolute z-10 top-5 right-5'
+              {...fadeAnimation}
+            >
+              <CustomButton 
+                type='filled'
+                title='Go Back'
+                handleClick={() => state.intro = true}
+                customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              />
+
+            </motion.div>
         </>
       )}
     </AnimatePresence>
