66f87bfddf3cc1c9437ffdd271290c2e213af879
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-04-17 13:51:55 -0400

    display shirt component

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -11,8 +11,21 @@ const Shirt = () => {
   const snap = useSnapshot(state)
   const { nodes, materials } = useGLTF('./shirt_baked.glb')
 
+  const logoTexture = useTexture(snap.logoDecal)
+  const fullTexture = useTexture(snap.fullDecal)
+
   return (
-    <div>Shirt</div>
+    <group>
+      <mesh
+        castShadow
+        geometry={nodes.T_Shirt_male.geometry}
+        material={materials.lambert1}
+        material-roughness={1}
+        dispose={null}
+      >
+
+      </mesh>
+    </group>
   )
 }
 
