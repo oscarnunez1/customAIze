77d315376f32441f2cb59b6f828481dfa81b6112
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-04-18 17:26:36 -0400

    add properties to color picker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,9 +1,26 @@
 // ColorPicker allows user to pick color of shirt
 import React from 'react'
+import { SketchPicker } from 'react-color'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
 
 const ColorPicker = () => {
+
+  const snap = useSnapshot(state)
+
   return (
-    <div>ColorPicker</div>
+    <div className='absolute left-full ml-3'>
+      <SketchPicker 
+        color={snap.color}
+        disableAlpha
+        presetColors={[
+          '#FF00FF', '#00FFFF', '#FFD700', '#EE82EE', '#FF4500', '#ADFF2F', '#32CD32', '#BA55D3', '#FF1493', '#7FFF00'
+        ]}
+        onChange={(color) => state.color = color.hex}
+
+      />
+    </div>
   )
 }
 
