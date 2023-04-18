9cf8651559f49e21527b9532dbd9a39d3a508dc2
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-04-18 14:14:22 -0400

    add properties to canvas component

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,25 +1,21 @@
 import React from 'react'
-import { easing } from 'maath'
-import { useSnapshot } from 'valtio'
-import { useFrame } from '@react-three/fiber'
-import { Decal, useGLTF, useTexture } from '@react-three/drei'
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
 
-import state from '../store'
+import state from '../store';
 
 const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb');
 
-  const snap = useSnapshot(state)
-  const { nodes, materials } = useGLTF('./shirt_baked.glb')
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
 
-  const logoTexture = useTexture(snap.logoDecal)
-  const fullTexture = useTexture(snap.fullDecal)
-  console.log(fullTexture);
+  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
 
-  useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, -.25, delta))
-
-  const stateString = JSON.stringify(snap)
-  console.log(stateString);
-
+  const stateString = JSON.stringify(snap);
 
   return (
     <group key={stateString}>
@@ -34,7 +30,7 @@ const Shirt = () => {
           <Decal 
             position={[0, 0, 0]}
             rotation={[0, 0, 0]}
-            scale={[1]}
+            scale={1}
             map={fullTexture}
           />
         )}
@@ -45,7 +41,7 @@ const Shirt = () => {
             rotation={[0, 0, 0]}
             scale={0.15}
             map={logoTexture}
-            map-anisotrapy={16}
+            map-anisotropy={16}
             depthTest={false}
             depthWrite={true}
           />
