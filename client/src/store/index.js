3bad852033894cb84f3d481fd04085860f0b5733
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-04-18 12:46:50 -0400

    render shirt

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { proxy } from "valtio"
 
 const state = proxy({
   intro: true,
-  color: '#EFBD48',
+  color: '#EFBD41',
   isLogoTexture: true,
   isFullTexture: false, 
   logoDecal: './threejs.png',
