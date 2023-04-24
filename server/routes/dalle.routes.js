bacfb718eeedbb0b4c5d4189a6487c98917fce85
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-04-24 14:31:25 -0400

    configure openai api

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -6,6 +6,12 @@ dotenv.config()
 
 const router = express.Router()
 
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(config)
+
 router.route('/').get((req, res) => {
   res.status(200).json({  message: "Hello from DALL.E ROUTES"})
 })
