5fcc0eb4c18bd1a8ddb38d2e8651b10276402fe9
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-04-24 14:39:07 -0400

    write out post controller

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -11,9 +11,30 @@ const config = new Configuration({
 })
 
 const openai = new OpenAIApi(config)
+console.log("IMAGE", openai);
 
 router.route('/').get((req, res) => {
   res.status(200).json({  message: "Hello from DALL.E ROUTES"})
+})
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json'
+    })
+
+    const image = response.data.data[0].b64_json
+
+    res.status(200).json({ photo: image })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({  message: "Something went wrong" })
+  }
 })
 
 export default router
